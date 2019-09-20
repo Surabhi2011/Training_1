a1a1f1bb5f2289693ec9b22f63ae1ae531fbe200
Surabhi Srivastava <surabhi.srivastava@synapse.com>
2019-09-20 16:20:53 +0530

    Added TestScript for Amazon Scenario

--- src/test/java/org/shl/testng/SHL_Prj1/TestScript3.java
+++ src/test/java/org/shl/testng/SHL_Prj1/TestScript3.java
@@ -1,9 +1,27 @@
 package org.shl.testng.SHL_Prj1;
 
+import java.lang.reflect.Method;
+
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class TestScript3 {
+import AmazonPages.HomePage;
+import AmazonPages.ProductPage;
+import utilities.Constants;
+import utilities.Initialiser;
+
+public class TestScript3 extends Initialiser {
+	@BeforeClass
+	public void makeObjects() {
+		driver.get(Constants.URL2);
+		amazonHomePage= new HomePage(driver, wait);
+		amazonProductPage=new ProductPage(driver,wait);
+	}
   @Test
-  public void f() {
+  public void TestThirdScenario(Method method) {
+	  amazonHomePage.search(Constants.MOBILE,method.getName());
+	  amazonProductPage.sortBy();
+	  amazonProductPage.selectItem();
+	 
   }
 }
