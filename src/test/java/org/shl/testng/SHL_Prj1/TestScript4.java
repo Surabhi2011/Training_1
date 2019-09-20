293ecaa3d2ab7544e73ae224fcf5d3a5f1524932
Surabhi Srivastava <surabhi.srivastava@synapse.com>
2019-09-20 12:39:33 +0530

    Added the testScripts for Scenario 4 and 5

--- src/test/java/org/shl/testng/SHL_Prj1/TestScript4.java
+++ src/test/java/org/shl/testng/SHL_Prj1/TestScript4.java
@@ -1,13 +1,28 @@
 package org.shl.testng.SHL_Prj1;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import ExcelUtilities.Scenario4;
+import ExcelUtilities.ReadExcel;
+import ExcelUtilities.WriteExcel;
+import utilities.Constants;
+import utilities.Launcher;
 
-public class TestScript4 {
-  @Test
-  public void readExcel() {
-	  Scenario4 scenario=new Scenario4();
-	  scenario.read_Excel();
-  }
+public class TestScript4 extends Launcher {
+	WebDriver driver;
+	WriteExcel writeExcel;
+	ReadExcel readExcel;
+	String[] data;
+   
+		@Test
+	public void testExcel() throws Exception {
+		
+		writeExcel = new WriteExcel();
+		writeExcel.createExcel(Constants.DATA_ARRAY1);
+		readExcel = new ReadExcel();
+		data = readExcel.read_Excel(2);
+		driver = launchBrowser(data[0]);
+		driver.get(data[1]);
+	}
 }
--- src/test/java/org/shl/testng/SHL_Prj1/TestScript5.java
+++ src/test/java/org/shl/testng/SHL_Prj1/TestScript5.java
@@ -1,13 +1,28 @@
 package org.shl.testng.SHL_Prj1;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
-import ExcelUtilities.Scenario5;
+import ExcelUtilities.ReadExcel;
+import ExcelUtilities.WriteExcel;
+import utilities.Constants;
+import utilities.Launcher;
 
-public class TestScript5 {
-  @Test
-  public void readExcel() {
-  Scenario5 scenario=new Scenario5();
-  scenario.read_Excel();
-  }
+public class TestScript5 extends Launcher {
+	WebDriver driver;
+	WriteExcel writeExcel;
+	ReadExcel readExcel;
+	String[] data;
+   
+		@Test
+	public void testExcel() throws Exception {
+		
+		writeExcel = new WriteExcel();
+		writeExcel.createExcel(Constants.DATA_ARRAY2);
+		readExcel = new ReadExcel();
+		data = readExcel.read_Excel(4);
+		for(int i=0;i<data.length;i++) {
+			System.out.println(data[i]);
+		}
+	}
 }
