8b48d3fb4ea5a6ada672a78ccc90e13e2535c0fb
Surabhi Srivastava <surabhi.srivastava@synapse.com>
2019-09-20 12:37:50 +0530

    Made Changes in Constant file for Scenario 4 and 5

--- src/main/java/utilities/Constants.java
+++ src/main/java/utilities/Constants.java
@@ -8,11 +8,14 @@ package utilities;
  */
 public class Constants {
 	public final static String BROWSER = "Chrome";
-	public final static String EXCELFILE = "C:\\Users\\surabhi.srivastava\\Desktop\\Example1.xlsx";
 	public final static String URL = "https://www.flipkart.com";
 	public final static String EARPHONES = "earphones";
 	public final static int NUMBER_OF_ELEMENTS = 3;
+	public final static String URL2 ="https://www.amazon.in";
+	public final static String MOBILE="mobile";
 	public final static String EXCELFILE1 = "C:\\Users\\surabhi.srivastava\\Desktop\\ExcelUtility1.xlsx";
 	public final static String EXCELFILE2 = "C:\\Users\\surabhi.srivastava\\Desktop\\ExcelUtility2.xlsx";
-
+    public final static int NUMBER_OF_ROWS=2;
+    public final static String[][] DATA_ARRAY1= {{"BROWSER","URL"},{"Chrome","https://www.flipkart.com"}};
+    public final static String[][] DATA_ARRAY2= {{"Name", "Company","D.O.B.","City"},{"Surabhi","GL","13-Aug-97","Kanpur"}};
 }
